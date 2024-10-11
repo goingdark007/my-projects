4091e613cb2a447526da244344cddbb80f2bf42f
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2024-10-11 15:39:48 +0600

    Updated pr1 Dictationary gradebook.py

--- pr1 Dictationary gradebook.py
+++ pr1 Dictationary gradebook.py
@@ -1,40 +1,47 @@
-student = {}
+student = {
 
-print('----Grade book----')
-print('Enter 1-5:\n1. Add student and grade\n2. View grade book\n3. Update a student\'s grade\n4. Delete a student\n5. Exit')
+}
+
+print(f"{'Grade Book':>{15}}")
+
+print('-'*20,'\n')
+
+print('Enter 1-5:\n\n1. Add a student and grade.\n\n2. View grade book.\n\n3. Update a student\'s grade.\n\n4. Delete a student.\n\n5. Exit.')
 
 def add():
-    a = input('Enter the student name: ').lower()
-    b = input('Enter the student grade: ')
+    a = input('\nEnter the student\'s name: ').upper()
+    b = input('\nEnter the student\'s grade: ').upper()
     student.update({a:b})
-    print('Successfully added')
+    print('\nThe student is successfully added to grade book')
 
 def view():
+    print(f"\n{'Key':<{15}} {'Value':<{15}}")
+    print('-'*30)
     for i, j in student.items():
-        print(i, '\t: ', j)
+        print(f'\n{i:<{15}} {j:<{15}}')
 
 def update():
-    a = input('Enter the name of the student: ').lower()
+    a = input('\nEnter the name of the student: ').upper()
     if a in student.keys():
-        b = input('Enter grade: ')
+        b = input('\nEnter the student\'s grade: ').upper()
         student.update({a:b})
-        print('Successfully updated')
+        print('\nThe student\'s grade is successfully updated to grade book')
     else:
-        print('Student name not found')
+        print('\nSorry, student name not found in the grade book')
 
 def delete():
-    a = input('Enter the name of the student: ').lower()
+    a = input('\nEnter the name of the student: ').upper()
     if a in student.keys():
         del student[a]
-        print('Successfully deleted')
+        print('\nThe student is successfully deleted from grade book')
     else:
-        print('Student name not found')
+        print('\nSorry, student name not found in the grade book')
 
 while True:
     try:
-        n = int(input('Enter: '))
+        n = int(input('\nEnter the operation you want to perform: '))
     except ValueError:
-        print("Oops! That wasn't a number. Please enter a valid number.")
+        print("\nOops! That wasn't a number. Please enter a valid number.")
         continue
 
     if n == 1:
@@ -46,7 +53,7 @@ while True:
     elif n == 4:
         delete()
     elif n == 5:
-        print('Exiting the program.\nThank you for using this program')
+        print('\n\nExiting the program.Thank you for using this program.')
         break
     else:
-        print('Invalid input')
+        print('Sorry, invalid input. Please enter between number 1-5.\n')
