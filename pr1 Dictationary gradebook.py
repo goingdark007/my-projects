d7171646f013baf74ff527aadbf937430ded4fc9
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2024-10-17 22:17:23 +0600

    Update pr1 Dictationary gradebook.py

--- pr1 Dictationary gradebook.py
+++ pr1 Dictationary gradebook.py
@@ -1,6 +1,13 @@
+# Grade book
 student = {
 
 }
+
+try:
+    with open('new.txt','x') as f:
+        f.write('')
+except FileExistsError:
+    pass
 
 print(f"{'Grade Book':>{15}}")
 
@@ -8,23 +15,37 @@ print('-'*20,'\n')
 
 print('Enter 1-5:\n\n1. Add a student and grade.\n\n2. View grade book.\n\n3. Update a student\'s grade.\n\n4. Delete a student.\n\n5. Exit.')
 
+with open('new.txt','r') as f:
+    for line in f:
+        if line.strip():
+            key = line[:15].strip()
+            value = line[15:].strip()
+            student[key] = value
+def file():
+    for i,j in student.items():
+        with open('new.txt','w') as f:
+            f.write(f'\n{i:<{15}}{j:<{15}}')
+
+
 def add():
     a = input('\nEnter the student\'s name: ').upper()
     b = input('\nEnter the student\'s grade: ').upper()
     student.update({a:b})
+    file()
     print('\nThe student is successfully added to grade book')
 
 def view():
     print(f"\n{'Key':<{15}} {'Value':<{15}}")
     print('-'*30)
-    for i, j in student.items():
-        print(f'\n{i:<{15}} {j:<{15}}')
+    with open('new.txt','r') as f:
+        print(f.read())
 
 def update():
     a = input('\nEnter the name of the student: ').upper()
-    if a in student.keys():
+    if a in student.keys() or check(a):
         b = input('\nEnter the student\'s grade: ').upper()
         student.update({a:b})
+        file()
         print('\nThe student\'s grade is successfully updated to grade book')
     else:
         print('\nSorry, student name not found in the grade book')
@@ -33,6 +54,7 @@ def delete():
     a = input('\nEnter the name of the student: ').upper()
     if a in student.keys():
         del student[a]
+        file()    
         print('\nThe student is successfully deleted from grade book')
     else:
         print('\nSorry, student name not found in the grade book')
